44b660b4949d63771903ec014fa448e0223c0db0
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-11-25 19:09:49 -0500

    Initial Commits

---
+++ Trying/tryingPClient.erl
@@ -0,0 +1,13 @@
+-module(tryingPClient).
+
+%Register Account
+
+%Send Tweet
+
+%Subscribe to User Tweets
+
+%Retweets
+
+%Live Tweets if connected.
+
+
---
+++ Trying/tryingPServer.erl
@@ -0,0 +1,6 @@
+-module(tryingPServer).
+
+% Register Account
+
+%Manage Tweets with Hashtag and subscriptions and mentions
+
