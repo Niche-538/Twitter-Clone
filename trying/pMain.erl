d5e98de4f4f8d034e771e358cbf1a8e660db0b43
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-11-29 14:50:07 -0500

    Server Loop Working

--- trying/pMain.erl
+++ trying/pMain.erl
@@ -3,7 +3,13 @@
 
 main() ->
   ServerID = spawn(pServer, server_fun, []),
-  io:fwrite("Server ID: ~p~n", [ServerID]).
+  io:fwrite("Server ID: ~p~n", [ServerID]),
+  NumClients = 10,
+  MaxSubscribers = 10,
+  ets:new(user_table, [set, named_table]),
+  ets:insert(user_table, {pratik, 10,10}),
+  ets:lookup(user_table, 1),
+  ServerID ! {atom}.
 
 
 
