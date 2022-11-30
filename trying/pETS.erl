feaf7b5289246d16530fe988830953382b1d858f
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-11-30 12:41:30 -0500

    Registration Working

--- trying/pETS.erl
+++ trying/pETS.erl
@@ -4,4 +4,9 @@
 %%Create Public ETS Tables
 create_tables()->
   ets:new(user_table, [set, public, named_table]),
+  ets:new(clients_registry, [set, public, named_table]),
+  ets:new(tweets, [set, public, named_table]),
+  ets:new(hashtags_mentions, [set, public, named_table]),
+  ets:new(subscribed_to, [set, public, named_table]),
+  ets:new(followers, [set, public, named_table]),
   io:fwrite("Tables Created ~n").
