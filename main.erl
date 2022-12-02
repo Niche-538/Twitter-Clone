67f283b480d5bf41bba23b5f7f270a455bb32333
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-12-01 21:42:18 -0500

    Adding Discoonection

--- main.erl
+++ main.erl
@@ -5,8 +5,9 @@ main(NumClients, SubscriberLimit, OfflineClientsPercentage) ->
     %%Create Server Engine Node
     ServerID = spawn(server, server_fun, []),
     io:fwrite("Server ID: ~p~n", [ServerID]),
-    _OfflineClients = (OfflineClientsPercentage * 0.01) * NumClients,
-    createUsers(NumClients, SubscriberLimit, ServerID, NumClients).
+    OfflineClients = (OfflineClientsPercentage * 0.01) * NumClients,
+    createUsers(NumClients, SubscriberLimit, ServerID, NumClients),
+    offlineSimulation(NumClients,OfflineClients,ServerID).
 
 createUsers(Counter, SubscriberLimit, ServerID, NumClients) ->
     if
@@ -23,3 +24,33 @@ createUsers(Counter, SubscriberLimit, ServerID, NumClients) ->
             ets:insert(main_table, {UserID, PID}),
             createUsers(Counter - 1, SubscriberLimit, ServerID, NumClients)
     end.
+
+
+offlineSimulation(NumClients, OfflineClients, ServerID) ->
+    DisconnectList = disconnection_handler(NumClients, OfflineClients, 0, [], ServerID),
+    lists:foreach(
+        fun(UserID) ->
+        PID = spawn(client, client_fun, [ServerID, UserID, _, _, _, 1]),
+        ets:insert(main_table, {UserID, PID})
+        end,
+        DisconnectList).
+
+
+
+disconnection_handler(NumClients, OfflineClients, DisconnectedClients, DisconnectedList, ServerID) ->
+    case OfflineClients < DisconnectedClients of
+          true->
+              OfflineClient = rand:uniform(NumClients),
+              {_, OfflineClientId} = ets:lookup(main_table, OfflineClient),
+              case OfflineClientId == nil of
+                    true -> disconnection_handler(NumClients, OfflineClients, DisconnectedClients, DisconnectedList,ServerID);
+                    false ->
+                        UpdatedList = lists:append(DisconnectedList, OfflineClient),
+                        ServerID ! {disconnectUser,{OfflineClient}},
+                        ets:insert(main_table, {OfflineClient, nil}),
+                        io:fwrite("User Id : ~p Disconnected ~n", [OfflineClient]),
+                        disconnection_handler(NumClients, OfflineClients, DisconnectedClients, UpdatedList,ServerID)
+
+                end;
+          false-> DisconnectedList
+end.
